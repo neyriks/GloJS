9dda09f384eb6ad04fe6fb732f398267c8aaa998
Neyriks <mesuppgg@Gmail.com>
2020-03-27 13:35:24 +0300

    Почистил js

--- lesson1/script/script.js
+++ lesson1/script/script.js
@@ -1,10 +1,8 @@
 let money, income, addExpenses, deposit, mission, period;
 
-<<<<<<< HEAD
+
 alert ('Hello world!');
 
 console.log ('Hi world!');
-=======
-alert('Hello world!');
-console.log('Hi world!');
->>>>>>> 82d26020f361d75ce5fef56232867889eba2a572
+
+
