8a5e67ddb0b3d8c59bb2147964447f3a9f88f52b
Neyriks <mesuppgg@Gmail.com>
2020-03-27 18:58:36 +0300

    Сделал 2ой урок

--- lesson1/script/script.js
+++ lesson1/script/script.js
@@ -1,8 +1,18 @@
-let money, income, addExpenses, deposit, mission, period;
+let money, income, addExpenses, deposit, mission, period, budgetDat;
+
+money = 50000;
+income = "фриланс";
+addExpenses = 'Интернет, Такси, Коммуналка';
+deposit = true;
+mission = 300000;
+period = 6;
 
 
-alert ('Hello world!');
+console.log (typeof money, income, deposit);
+console.log (addExpenses.length);
+console.log ('Период равен ' + period + ' месяцев' + ' и' + ' Цель заработать: '+ mission + ' рублей');
 
-console.log ('Hi world!');
+console.log (addExpenses.split(', ') && addExpenses.toLowerCase());
 
-
+budgetDay = money  / 30;
+console.log(budgetDay);
