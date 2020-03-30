4f9187e3dfc0ef51ecdbdfb2eaf6fa7b05eb29d9
neyriks <mesuppgg@gmail.com>
2020-03-31 00:46:40 +0300

    Исправил money->budgetMonth и поставил в конце логи

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ let money = prompt('Ваш месячный доход?'),
     deposit = confirm('Есть ли у вас депозит в банке?'),
     mission = 60000,
     period = 6,
-    budgetDay = money  / 30,
     expenses1 = prompt('Введите обязательную статью расходов?'),
     amount1 = prompt('Во сколько это обойдется?'),
     expenses2 = prompt('Введите обязательную статью расходов?'),
     amount2 = prompt('Во сколько это обойдется?'),
-    budgetMonth = money - amount1 - amount2;
+    budgetMonth = money - amount1 - amount2,
+    budgetDay = budgetMonth  / 30;
+
+
+// Конструкция if
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 && budgetDay > 0) {
+    console.log('К сожалению, у вас уровень дохода ниже среднего');
+} else if (budgetDay <= 0) {
+    console.log('Что-то пошло не так');
+}
+
 
 
 // Вывод в консоль
@@ -24,13 +37,3 @@ console.log('Ваш бюджет на месяц:' + budgetMonth);
 console.log('Цель будет достигнута за:' + Math.ceil(mission / budgetMonth));
 console.log('Бюджет на день:' + Math.floor(budgetMonth/30));
 
-// Конструкция if
-if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600 && budgetDay < 1200) {
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay < 600 && budgetDay > 0) {
-    console.log('К сожалению, у вас уровень дохода ниже среднего');
-} else if (budgetDay <= 0) {
-    console.log('Что-то пошло не так');
-}
