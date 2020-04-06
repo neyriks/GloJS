2a87fb2479db0a4e7577b0534ebe41724ea41036
neyriks <mesuppgg@gmail.com>
2020-04-06 23:40:42 +0300

    Урок 7 правки

--- script.js
+++ script.js
@@ -10,9 +10,8 @@ let money,
     };
 start();
 let expense,
-    count;
-
-let appData = {
+    count,
+    appData = {
     income: {},
     addIncome: [],
     expenses: {},
@@ -30,32 +29,30 @@ let appData = {
             appData.deposit = confirm('Есть ли у вас депозит в банке?');
     },
     getExpensesMonth: function () {  
-        for (let i = 0; i < 2; i++) {    // Не сохраняет две переменные, а только одну.
+        for (let i = 0; i < 2; i++) { 
             let ask = prompt('Введите обязательную статью расходов');         
             let count = prompt('Во сколько это обойдется?');
-            appData.expenses[ask] = +count;
             while(!isNumber(count)) {
                 count = prompt('Во сколько это обойдется?');
             }
+            appData.expenses[ask] = +count;
         }
-
-       for(let key in appData.expenses) {   // Правильно или нет? Не могу проверить, потому-что выше не сохранились два значения.
+       for(let key in appData.expenses) {   
             appData.expensesMonth += +appData.expenses[key];
-            return appData.expensesMonth;
        }
     },   
-    getBudget: function() {   // getAccumulatedMonth переименовать в getBudget. Этот метод будет высчитывать значения свойств budgetMonth и budgetDay, чтобы вычислить значения используем только свойства объекта (никаких внешних переменных)
-        appData.budgetDay = (appData.budgetDay / 30);
-        appData.budgetMonth =(money - appData.expenses);
+    getBudget: function() {  
+        appData.budgetDay = (money - appData.expensesMonth) / 30;
+        appData.budgetMonth = money - appData.expensesMonth;
     },
-    getTargetMonth: function() { // В методах getTargetMonth и getStatusIncome исправить переменные, все значения получаем от нашего объекта appData
+    getTargetMonth: function() { 
         if (appData.mission/appData.budgetMonth < 0){
-            console.log ('Цель не будет достигнута');
+            return 'Цель не будет достигнута';
         } else {
            return appData.mission / appData.budgetMonth; 
         }
     },
-    getStatusIncome: function() { //В методах getTargetMonth и getStatusIncome исправить переменные, все значения получаем от нашего объекта appData
+    getStatusIncome: function() { 
         if (appData.budgetDay >= 1200) {
             return ('У вас высокий уровень дохода');
         } else if (appData.budgetDay >= 600 && appData.budgetDay < 1200) {
@@ -73,15 +70,11 @@ appData.getBudget();
 appData.getTargetMonth();
 appData.getStatusIncome();
 
-
-
-
 // Вывод в консоль
-for(let key in appData) {
-    console.log("Наша программа включает в себя данные: " + key);
-}
 console.log (Object.keys(appData).length);
 console.log('Ваши расходы за месяц: ' + appData.expensesMonth);
 console.log('Цель будет достигнута за:' + Math.ceil(appData.getTargetMonth()));
-console.log('Ваш уровень дохода: ' + appData.getStatusIncome);   //Выводит функцию, не должно.
-
+console.log('Ваш уровень дохода: ' + Math.ceil(appData.getStatusIncome));
+for(let key in appData) {
+    console.log("Наша программа включает в себя данные: " + key);
+}
