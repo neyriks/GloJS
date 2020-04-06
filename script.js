d90f252792f0f45dffb2ef3ea49057d404d6b95b
neyriks <mesuppgg@gmail.com>
2020-04-06 18:35:46 +0300

    7ой урок, все задания, на проверку и дальнейшие правки

--- script.js
+++ script.js
@@ -9,10 +9,8 @@ let money,
         } while (!isNumber(money));
     };
 start();
-let expenses1, 
-    expenses2, 
-    accumulatedMonth,
-    budgetDay = accumulatedMonth  / 30;
+let expense,
+    count;
 
 let appData = {
     income: {},
@@ -31,64 +29,59 @@ let appData = {
             appData.addExpenses = addExpenses.toLowerCase().split('');
             appData.deposit = confirm('Есть ли у вас депозит в банке?');
     },
-    getExpensesMonth: function (amount1, amount2) {  
-        let sum = 0;
-        for(let i = 0; i < 2; i++) {
-            if(i === 0) {
-                expenses1 = prompt('Введите обязательную статью расходов?');
-            } else if(i === 1) {
-                expenses1 = prompt('Введите обязательную статью расходов?');
-            }
-    
+    getExpensesMonth: function () {  
+        for (let i = 0; i < 2; i++) {    // Не сохраняет две переменные, а только одну.
+            let ask = prompt('Введите обязательную статью расходов');         
             let count = prompt('Во сколько это обойдется?');
-    
+            appData.expenses[ask] = +count;
             while(!isNumber(count)) {
                 count = prompt('Во сколько это обойдется?');
             }
-            sum += +count;
         }
-        return sum;
+
+       for(let key in appData.expenses) {   // Правильно или нет? Не могу проверить, потому-что выше не сохранились два значения.
+            appData.expensesMonth += +appData.expenses[key];
+            return appData.expensesMonth;
+       }
+    },   
+    getBudget: function() {   // getAccumulatedMonth переименовать в getBudget. Этот метод будет высчитывать значения свойств budgetMonth и budgetDay, чтобы вычислить значения используем только свойства объекта (никаких внешних переменных)
+        appData.budgetDay = (appData.budgetDay / 30);
+        appData.budgetMonth =(money - appData.expenses);
     },
-    getAccumulatedMonth: function() {
-        let expensesAmount = appData.getExpensesMonth();
-        return money - expensesAmount;
-    },
-    getTargetMonth: function() {
-        if (appData.mission/accumulatedMonth < 0){
+    getTargetMonth: function() { // В методах getTargetMonth и getStatusIncome исправить переменные, все значения получаем от нашего объекта appData
+        if (appData.mission/appData.budgetMonth < 0){
             console.log ('Цель не будет достигнута');
         } else {
-           return appData.mission / accumulatedMonth; 
+           return appData.mission / appData.budgetMonth; 
         }
     },
-    getStatusIncome: function() {
-        if (budgetDay >= 1200) {
+    getStatusIncome: function() { //В методах getTargetMonth и getStatusIncome исправить переменные, все значения получаем от нашего объекта appData
+        if (appData.budgetDay >= 1200) {
             return ('У вас высокий уровень дохода');
-        } else if (budgetDay >= 600 && budgetDay < 1200) {
+        } else if (appData.budgetDay >= 600 && appData.budgetDay < 1200) {
             return ('У вас средний уровень дохода');
-        } else if (budgetDay < 600 && budgetDay > 0) {
+        } else if (appData.budgetDay < 600 && appData.budgetDay > 0) {
             return ('К сожалению, у вас уровень дохода ниже среднего');
-        } else if (budgetDay <= 0) {
+        } else if (appData.budgetDay <= 0) {
             return ('Что-то пошло не так');
         }
     }
 };
-
-appData.getAccumulatedMonth();
-
-accumulatedMonth = appData.getAccumulatedMonth();
-
+appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
 appData.getTargetMonth();
-
 appData.getStatusIncome();
 
 
 
 
 // Вывод в консоль
-console.log (appData.addExpenses.length);
-console.log ('Период равен ' + appData.period + ' месяцев и Цель заработать: '+ appData.mission + ' рублей');
-console.log (appData.addExpenses);
-console.log(Math.floor(budgetDay));
-console.log('Ваш бюджет на месяц:' + accumulatedMonth);
+for(let key in appData) {
+    console.log("Наша программа включает в себя данные: " + key);
+}
+console.log (Object.keys(appData).length);
+console.log('Ваши расходы за месяц: ' + appData.expensesMonth);
 console.log('Цель будет достигнута за:' + Math.ceil(appData.getTargetMonth()));
-console.log('Бюджет на день:' + Math.floor(budgetDay));
+console.log('Ваш уровень дохода: ' + appData.getStatusIncome);   //Выводит функцию, не должно.
+
